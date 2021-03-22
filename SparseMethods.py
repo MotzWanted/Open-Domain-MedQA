2585330788da2c1f0cc63d5a81f1043f9561f8b7
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2021-03-22 18:39:15 +0100

    IR-ES Custom attempt

--- SparseMethods.py
+++ SparseMethods.py
@@ -1,100 +1,91 @@
 import math
 import nltk
 from nltk import FreqDist
+from nltk.stem import SnowballStemmer
 from tqdm import tqdm
 import pickle
+import numpy as np
+stemmer = SnowballStemmer("english")
 
+import os
+dirname = os.path.dirname(__file__)
 
-# helper functions for n-grams used by MedQA
-def gen_bigrams(tokens):
-    # returns a tuple
-    return nltk.bigrams(tokens)
+class SparseMethods:
+    def __init__(self, Document_Frequency_Distributions, avgQueryLen, avgDocLen, ngram_func):
+        self.ngram_func = ngram_func
+        self.avgQueryLen = avgQueryLen
+        self.avgDocLen = avgDocLen
+        self.Doc_Freq_Dists = Document_Frequency_Distributions
 
+    # static helper functions for n-grams used by MedQA
+    @staticmethod
+    def gen_unigrams(tokens):
+        return [(token) for token in tokens]
 
-def gen_trigrams(tokens):
-    return nltk.trigrams(tokens)
+    @staticmethod
+    def gen_bigrams(tokens):
+        return nltk.bigrams(tokens)
 
+    @staticmethod
+    def gen_trigrams(tokens):
+        return nltk.trigrams(tokens)
 
-def gen_skipbigrams(tokens):
-    return nltk.skipgrams(tokens, 2, 1)
-
-
-# IR functions
-def f(q_i, Q_or_D_FreqDist):
-    # from a n-gram frequency distribution return q_i frequency if any
-    return Q_or_D_FreqDist[q_i]
-
-
-def IDF(q_i):
-    # takes in an n-gram tuple
-    N = len(q_i)
-    # instantiate a dictionary of frequencies
-    dictionary = dict()
-    for word in q_i:
-        if word in dictionary:
-            dictionary[word] += 1
-        else:
-            dictionary[word] = 1
-    # apply idf from frequency
-    for word, val in dictionary.items():
-        dictionary[word] = math.log(N / float(val))
-    return dictionary
-
-
-def s(Q, D):
-    # Q is a collection of query n-grams
-    # D is a n-gram frequency distributions
-    # Note: D should be precomputed
-
-    # hyper-parameters (USMLE)
-    k_Q = 0.40
-    b_Q = 0.70
-    k_D = 0.90
-    b_D = 0.35
-
-    results = []
-    for q_i in tqdm(Q):
-        # terms
-        idf = IDF(q_i)
-        f_q = f(q_i, Q)
-        f_d = f(q_i, D)
-        queryLen = len(q_i)
-        docLen = len(D[q_i])  # freq of q_i n-gram in Document
-        doc_samples = []
-        for ngram, freq in D.items():
-            doc_samples.append(freq)
-        avgDocLen = sum(doc_samples) / len(doc_samples)
-        q_samples = []
-        for ngram, freq in Q.items():
-            q_samples.append(freq)
-        avgQueryLen = sum(q_samples) / len(q_samples)
-
-        # compute bm25 from equation defined by the MedQA paper
-        bm25 = (IDF(q_i) * f(q_i, D) * (k_D + 1)) / (f(q_i, Q) +
-                                                     k_D * (1 - b_D + (b_D * (docLen / avgDocLen))))
-
-        # compute intermidiate s result
-        s = (bm25 * idf * f_q * (k_Q + 1)) / (f_q + k_Q *
-                                              (1 - b_Q + b_Q * (queryLen / avgQueryLen)))
-        results.append(s)
-
-    return sum(results)
-
-
-data_path = "/Users/andreasmotz/Documents/DTU/Kandidat/4. Semester/MEDQA/data_clean/dicts/"
-output_folder = "/Users/andreasmotz/Documents/DTU/Kandidat/4. Semester/MEDQA/data_clean/textbooks/en_sentence/"
-def benchmark():
-    train = ""
-    phys = ""
-    with open(data_path + '/train.pickle',"rb") as file:
-        train = pickle.load(file)
-    with open(output_folder + 'Physiology_Levy.pkl',"rb") as file:
-        phys = pickle.load(file)
+    @staticmethod
+    def gen_skipbigrams(tokens):
+        return nltk.skipgrams(tokens, 2, 1)
     
-    Q = nltk.bigrams(train["q1"]["qst"])
-    print(Q)
-    D = nltk.FreqDist(nltk.bigrams(phys))
+    @staticmethod
+    def prep_query(string):
+        tokenized = nltk.word_tokenize(string)
+        return [stemmer.stem(e) for e in tokenized]
 
-    print(s(Q,D))
+    @staticmethod
+    def prep_document(string):
+        tokenized = nltk.word_tokenize(string)
+        return [stemmer.stem(e) for e in tokenized]
+    
+    @staticmethod
+    def ngram_freq_dist(tokens, ngram_func):
+        return nltk.FreqDist(ngram_func(tokens))
+    
+    @staticmethod
+    def snowball_stem(tokens):
+        return stemmer.stem(tokens)
+    
+    def f(self, q_i, QorD):
+        # from a n-gram frequency distribution return q_i frequency if any
+        return QorD[q_i]
 
-benchmark()
+    def IDF(self, q_i):
+        N = len(self.Doc_Freq_Dists)
+        df_t = 0  # documents containing the n-gram
+        for _, freq_dist in enumerate(self.Doc_Freq_Dists):
+            if q_i in freq_dist:
+                df_t += 1
+                continue
+        return math.log(N/(df_t+1))
+
+    def S(self, Q, D):
+        queryLen = len(Q)
+        docLen = len(D)
+        Q = nltk.FreqDist(self.ngram_func(Q))
+        D = nltk.FreqDist(self.ngram_func(D))
+        # hyper-parameters (USMLE)
+        k_Q = 0.40
+        b_Q = 0.70
+        k_D = 0.90
+        b_D = 0.35
+        results = []
+        for q_i, _ in Q.items():
+            # terms
+            idf = self.IDF(q_i)
+            f_q = self.f(q_i, Q)
+            f_d = self.f(q_i, D)
+            # compute bm25 from equation defined by the MedQA paper
+            bm25 = (idf * f_d * (k_D + 1)) / (f_q + k_D *
+                                              (1 - b_D + (b_D * (docLen / self.avgDocLen))))
+            # compute intermidiate s result
+            s = (bm25 * idf * f_q * (k_Q + 1)) / (f_q + k_Q *
+                                                  (1 - b_Q + b_Q * (queryLen / self.avgQueryLen)))
+            results.append(s)
+        return sum(results)
