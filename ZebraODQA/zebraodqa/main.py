e92a28723cbb17f3302756ea8f50d613c81d7911
Benjamin Starostka Jakobsen <benjamin.starostka@gmail.com>
2021-05-05 17:16:41 +0200

    seperate commands

--- ZebraODQA/zebraodqa/main.py
+++ ZebraODQA/zebraodqa/main.py
@@ -9,9 +9,12 @@ from transformers import BertModel, PreTrainedTokenizerFast, BertTokenizer, DPRC
     DPRContextEncoderTokenizer, pipeline, DPRQuestionEncoder, BertTokenizerFast, AutoTokenizer
 from datasets import load_dataset
 import torch
+from commands.ingest import ingest
 from alive_progress import alive_bar
 
 app = typer.Typer()
+app.add_typer(ingest.app, name="ingest")
+
 state = {"verbose": False}
 Config = confuse.Configuration('ZebraODQA', __name__)
 
@@ -29,47 +32,6 @@ def corpus(model: str):
     Main corpus command!
     """
     return 0
-
-## Finetuning section
-@app.command()
-def finetune(freezeLayers: bool, config: bool):
-    """
-    Training command command!
-    """
-    model = BertModel.from_pretrained('bert-base-uncased' if not config else Config['model'].get())
-    if torch.cuda.is_available():
-        model = model.to(torch.device("device"))
-
-
-@app.command()
-def ingest():
-    """
-    Every model from HugginFace is applicable
-    TODO: put url here
-    Corpus example: squad | MedQA or FindZebra
-    """
-    typer.secho("Welcome to the ingest command", fg=typer.colors.WHITE, bold=True)
-
-    model = BertModel.from_pretrained(Config['model'].get())
-    fast_tokenizer = PreTrainedTokenizerFast.from_pretrained(Config['tokenizer'].get())
-    #fast_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-
-    corpus = load_dataset(Config['corpus'].get(), split='train[:100]') # cache_dir=Config['cache_dir'].get() -- Cache directory override
-
-    torch.set_grad_enabled(False)
-
-    typer.secho("Embedding corpus as dense context vector representation using FAISS.")
-    corpus_embeddings = corpus.map(
-        lambda example: {'embeddings': model(**fast_tokenizer(example['line'], return_tensors='pt'))['pooler_output'][0].numpy()})
-    # corpus_embeddings.save_to_disk(os.path.join(Config['cache_dir'].get(), "corpus/"))
-
-    typer.secho("Adding FAISS index for efficient similarity search and clustering of dense vectors.")
-    corpus_embeddings.add_faiss_index(column='embeddings')
-
-    typer.secho("Saving the index")
-    # corpus_embeddings.save_faiss_index("embeddings", os.path.join(Config['cache_dir'].get(), "corpus.faiss"))
-    return 0
-
 
 @app.command()
 def config_template():
