b8e894b6802db001b74a1d8c1ba1302d1bb28152
MotzWanted <36195371+MotzWanted@users.noreply.github.com>
2021-05-28 17:06:38 +0200

    Update colbert_data_constructor.py

--- utils/colbert_data_constructor.py
+++ utils/colbert_data_constructor.py
@@ -1,6 +1,7 @@
 import json, os, re, requests
 from utils.es_functions import *
 from tqdm import tqdm
+import gdown
 
 def getDocChunks(article, chunkSize=100, stride=50):
     article = article.replace('\n', ' ').replace('\r', '')
@@ -23,12 +24,26 @@ def is_positive(document, answer, synonyms):
 
     return False
 
-dev_url = 'https://raw.githubusercontent.com/MotzWanted/Open-Domain-MedQA/master/data/dataset/dev.json'
-dev_resp = requests.get(dev_url)
-dev = json.loads(dev_resp.text)
+train_url = 'https://drive.google.com/uc?id=1WZFwLpM_2RNHP2QE-JHlCm5mcb7I0FtN'
+dev_url = 'https://drive.google.com/uc?id=16sJUgYCVwYSp5Zy35xW7NlUUBGhDNdWO'
+test_url = 'https://drive.google.com/uc?id=1WZFwLpM_2RNHP2QE-JHlCm5mcb7I0FtN'
 
-datasets = [dev]
-ds_names = ["dev"]
+output = ['data/dataset/train.json', 'data/dataset/dev.json', 'data/dataset/test.json']
+gdown.download(train_url, output[0], quiet=False)
+gdown.download(dev_url, output[1], quiet=False)
+gdown.download(test_url, output[2], quiet=False)
+
+with open('data/dataset/train.json',"rb") as f:
+    train = json.load(f)
+
+with open('data/dataset/dev.json',"rb") as f:
+    val = json.load(f)
+
+with open('data/dataset/test.json',"rb") as f:
+    test = json.load(f)
+
+datasets = [train, val, test]
+ds_names = ["train", "val","test"]
 
 counter = 0
 
@@ -37,7 +52,7 @@ for ds_id, ds in enumerate(datasets):
         'version': '0.0.1',
         'data': []
         }
-    for key in tqdm(list(ds.keys())[:100]):
+    for key in tqdm(ds.keys()):
 
         if ds[key]['FZ_results']:
             q_id = int(key[1:]) 
@@ -45,7 +60,7 @@ for ds_id, ds in enumerate(datasets):
             is_golden = False
             synonyms = set()
             length_ = 0
-            answer_options = [dev[key]['answer_options'][opt] for opt in dev[key]['answer_options'].keys()]
+            answer_options = [ds[key]['answer_options'][opt] for opt in ds[key]['answer_options'].keys()]
 
             for article in ds[key]['FZ_results']:
                 synonyms.update(article['synonyms'])
@@ -55,21 +70,19 @@ for ds_id, ds in enumerate(datasets):
                 for doc in docs:
                     _ = es_ingest(q_id, article['title'], doc)
             
-            #print(length_)
-            es_res = es_search(q_id, dev[key]['question'], length_)
+            es_res = es_search(q_id, ds[key]['question'], length_)
 
             for hit in es_res['hits']:
-                #print(hit)
                 counter += 1
                 if is_golden==False:
-                    is_golden = is_positive(hit['_source']['text'], dev[key]['answer'], synonyms)
+                    is_golden = is_positive(hit['_source']['text'], ds[key]['answer'], synonyms)
                     
                     out['data'].append({
                         'idx' : counter,
                         'question_id' : q_id,
-                        'question' : dev[key]['question'],
+                        'question' : ds[key]['question'],
                         'answer_choices' : answer_options,
-                        'answer_idx' : answer_options.index(dev[key]['answer']),
+                        'answer_idx' : answer_options.index(ds[key]['answer']),
                         'document' : hit['_source']['title'] + ' ' + hit['_source']['text'],
                         'is_gold' : is_golden
                     })
@@ -78,15 +91,14 @@ for ds_id, ds in enumerate(datasets):
                     out['data'].append({
                             'idx' : counter,
                             'question_id' : q_id,
-                            'question' : dev[key]['question'],
+                            'question' : ds[key]['question'],
                             'answer_choices' : answer_options,
-                            'answer_idx' : answer_options.index(dev[key]['answer']),
+                            'answer_idx' : answer_options.index(ds[key]['answer']),
                             'document' : hit['_source']['title'] + ' ' + hit['_source']['text'],
                             'is_gold' : False
                         })
 
             es_remove_index(q_id)
-            #print(out['data'])
 
     with open(ds_names[ds_id]+'_FZ-MedQA.json',"w") as file:
         json.dump(out, file, indent=6)
